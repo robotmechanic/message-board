56e280b623933b3cca9a833bd9ac49eac8c83c5a
David H <dhovanky@gmail.com>
2016-02-11 08:09:21 +0000

    added edit and delte functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'messages#index'
+  resources :messages , except: [:index, :new]
   get 'messages/index'
   resources :messages
-  root :to => 'messages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
