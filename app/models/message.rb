ab9cb9da0c18a1c2ffa9e0e1804dcd14250e5d54
David H <dhovanky@gmail.com>
2016-02-17 07:35:28 +0000

    allow 0 age

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
-validates :age , numericality: { only_integer: true, greater_than: 0 }  # 内容は必須入力かつ2文字以上30文字以下
+　validates :age , numericality: { only_integer: true, greater_than_or_equal: 0 }  # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
